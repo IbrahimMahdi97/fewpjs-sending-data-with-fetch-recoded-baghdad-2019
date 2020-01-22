a83b2f7dacd0529934312be4221ce1977a545e11
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-22 17:46:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+  const requestOptions = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringfy({name: name, email: email})
+  }
+  return fetch (url, requestOptions)
+}
+submitData("John", "john@invisible.com")
+.then(res => {
+  return res.json
+})
+.then (json => {
+  let elem = document.createElemnt("div")
+  elem.innerHTML = json
+  document.body.appendChile (elem)
+  document.body.innerHTML = json_id_
+}).catch(err => {
+  document.body.innerHTML = err.message
+})
